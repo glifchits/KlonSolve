37ba3c7f4e96667d9a028cd7a304632fbf763076
George Lifchits <george.lifchits@gmail.com>
2019-10-25 17:18:59 -0400

    test different game and implement move function

--- tuplestate.py
+++ tuplestate.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 KlonState = namedtuple(
     "KlonState",
     [
+        "stock",
         "waste",
         "tableau1",
         "tableau2",
@@ -12,13 +13,26 @@ KlonState = namedtuple(
         "tableau5",
         "tableau6",
         "tableau7",
-        "stock",
         "foundation1",
         "foundation2",
         "foundation3",
         "foundation4",
     ],
 )
+
+STOCK = 0
+WASTE = 1
+TABLEAU1 = 2
+TABLEAU2 = 3
+TABLEAU3 = 4
+TABLEAU4 = 5
+TABLEAU5 = 6
+TABLEAU6 = 7
+TABLEAU7 = 8
+FOUNDATION_C = 9
+FOUNDATION_D = 10
+FOUNDATION_S = 11
+FOUNDATION_H = 12
 
 
 def init_from_solvitaire(game_dict):
@@ -31,9 +45,10 @@ def init_from_solvitaire(game_dict):
 
     stock = tuple(map(card, game_dict["stock"]))
     tableau = tuple(tuple(map(card, t)) for t in game_dict["tableau piles"])
-    waste = tuple(game_dict["waste"])
+    waste = tuple(game_dict.get("waste", ()))
     foundation = tuple(game_dict.get("foundations", [tuple()] * 4))
     return KlonState(
+        stock=stock,
         waste=waste,
         tableau1=tableau[0],
         tableau2=tableau[1],
@@ -42,7 +57,6 @@ def init_from_solvitaire(game_dict):
         tableau5=tableau[4],
         tableau6=tableau[5],
         tableau7=tableau[6],
-        stock=stock,
         foundation1=foundation[0],
         foundation2=foundation[1],
         foundation3=foundation[2],
@@ -50,12 +64,51 @@ def init_from_solvitaire(game_dict):
     )
 
 
+def last_face_up(pile):
+    return pile[:-1] + (pile[-1].upper(),)
+
+
+def move(state, src_pile, dest_pile, cards=1):
+    new_src = last_face_up(state[src_pile][:-cards])
+    new_dest = state[dest_pile] + state[src_pile][-cards:]
+    new_state = list(state)
+    new_state[src_pile] = new_src
+    new_state[dest_pile] = new_dest
+    return KlonState(*new_state)
+
+
 import unittest
 import json
 
 
 class TestState(unittest.TestCase):
+    def setUp(self):
+        initial_stock = (
+            "KC,9C,QC,8C,3C,7S,7H,TC,4C,7D,KS,AD,QS,KH,QD,TH,8S,AH,6H,4H,AS,2D,TD,3H"
+        )
+        self.state = KlonState(
+            stock=tuple(initial_stock.split(",")),
+            waste=(),
+            tableau1=("8H",),
+            tableau2=("3d", "6C"),
+            tableau3=("5s", "jd", "JS"),
+            tableau4=("2h", "kd", "7c", "9S"),
+            tableau5=("qh", "8d", "jc", "2c", "AC"),
+            tableau6=("4s", "6s", "2s", "3s", "9d", "5C"),
+            tableau7=("5h", "6d", "5d", "4d", "ts", "9h", "JH"),
+            foundation1=(),
+            foundation2=(),
+            foundation3=(),
+            foundation4=(),
+        )
+
     def test_init_from_solvitaire(self):
+        with open("./fixtures/sm-seed12.json") as f:
+            game1 = json.load(f)
+        state1 = init_from_solvitaire(game1)
+        self.assertEqual(state1.waste, ())
+        self.assertEqual(state1.tableau1, ("8H",))
+
         with open("./fixtures/game1.json") as f:
             game = json.load(f)
         state = init_from_solvitaire(game)
@@ -92,6 +145,13 @@ class TestState(unittest.TestCase):
         self.assertEqual(state.foundation2, ())
         self.assertEqual(state.foundation3, ())
         self.assertEqual(state.foundation4, ())
+
+    def test_move_tableau_to_foundation(self):
+        self.assertEqual(self.state[TABLEAU5], ("qh", "8d", "jc", "2c", "AC"))
+        self.assertEqual(self.state[FOUNDATION_C], ())
+        state2 = move(self.state, TABLEAU5, FOUNDATION_C)
+        self.assertEqual(state2[TABLEAU5], ("qh", "8d", "jc", "2C"))
+        self.assertEqual(state2[FOUNDATION_C], ("AC",))
 
 
 if __name__ == "__main__":
