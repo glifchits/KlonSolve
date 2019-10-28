feccb396ebd47234b259704e506b892e38d9dc25
George Lifchits <george.lifchits@gmail.com>
2019-10-28 15:18:00 -0400

    (non-deterministic) solver test, work on solver API, requirements.txt

--- solver.py
+++ solver.py
@@ -42,8 +42,6 @@ visited = set()
 
 def solve(state, move_seq):
     if state_is_win(state):
-        print("WIN")
-        print(list(move_seq))
         return True
     elif state in visited:
         return False
@@ -52,53 +50,5 @@ def solve(state, move_seq):
     for move_code in child_moves:
         child_state = play_move(state, move_code)
         if solve(child_state, append(move_seq, move_code)):
-            return True
+            return move_seq
     return False
-
-
-if __name__ == "__main__":
-    print("running solver")
-    game = {
-        "foundation": [[], [], [], []],
-        "waste": [],
-        "stock": [
-            "kc",
-            "9c",
-            "qc",
-            "8c",
-            "3c",
-            "7s",
-            "7h",
-            "10c",
-            "4c",
-            "7d",
-            "ks",
-            "ad",
-            "qs",
-            "kh",
-            "qd",
-            "10h",
-            "8s",
-            "ah",
-            "6h",
-            "4h",
-            "as",
-            "2d",
-            "10d",
-            "3h",
-        ],
-        "tableau": [
-            ["8H"],
-            ["3d", "6C"],
-            ["5s", "jd", "JS"],
-            ["2h", "kd", "7c", "9S"],
-            ["qh", "8d", "jc", "2c", "AC"],
-            ["4s", "6s", "2s", "3s", "9d", "5C"],
-            ["5h", "6d", "5d", "4d", "10s", "9h", "JH"],
-        ],
-    }
-    start = time.time()
-    ret = solve(init_from_ui_state(game), ())
-    end = time.time()
-    print(f"finished in {end-start:.2f} s")
-    print(f"solved: {ret}")
---
+++ test_solver.py
@@ -0,0 +1,62 @@
+import unittest
+import timeout_decorator
+from timeout_decorator import TimeoutError
+from solver import solve
+from tuplestate import init_from_ui_state
+
+
+@timeout_decorator.timeout(2)
+def solve_under_2sec(initial_state):
+    solution = solve(initial_state, ())
+    return solution
+
+
+class TestSolver(unittest.TestCase):
+    def test_a_game(self):
+        game = {
+            "foundation": [[], [], [], []],
+            "waste": [],
+            "stock": [
+                "kc",
+                "9c",
+                "qc",
+                "8c",
+                "3c",
+                "7s",
+                "7h",
+                "10c",
+                "4c",
+                "7d",
+                "ks",
+                "ad",
+                "qs",
+                "kh",
+                "qd",
+                "10h",
+                "8s",
+                "ah",
+                "6h",
+                "4h",
+                "as",
+                "2d",
+                "10d",
+                "3h",
+            ],
+            "tableau": [
+                ["8H"],
+                ["3d", "6C"],
+                ["5s", "jd", "JS"],
+                ["2h", "kd", "7c", "9S"],
+                ["qh", "8d", "jc", "2c", "AC"],
+                ["4s", "6s", "2s", "3s", "9d", "5C"],
+                ["5h", "6d", "5d", "4d", "10s", "9h", "JH"],
+            ],
+        }
+        state = init_from_ui_state(game)
+        solution = solve_under_2sec(state)
+        self.assertNotEqual(solution, None, "timed out, or erroneously returned None")
+        self.assertNotEqual(solution, False, "game incorrectly deemed unsolvable")
+
+
+if __name__ == "__main__":
+    unittest.main()
